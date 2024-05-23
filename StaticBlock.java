e2dbdb0e3d50b4be38e14d2ee20637bcd5a05fd6
Navya <bajpainavya8@gmail.com>
2024-05-23 17:46:20 +0530

    first commit

---
+++ StaticBlock.java
@@ -0,0 +1,12 @@
+class StaticExample { 
+
+    // Static block
+    static { 
+        System.out.println("Inside Static Block."); 
+    }
+
+    // Main method
+    public static void main(String args[]) { 
+        System.out.println("Inside main method."); 
+    } 
+}
